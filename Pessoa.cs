c41509f059f9632944529c12b560d015532aa6f2
thomasbrasil46 <83769834+thomasbrasil46@users.noreply.github.com>
2022-07-20 10:12:08 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    public class Pessoa
+    {
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+            set { this.anoNascimento = value; }
+        }
+
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        public void ExibirDados ()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Ano de nascimento: " + this.AnoNascimento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: " + idade);
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.AnoNascimento;
+            return idade;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando a idade de uma pessoa");
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Nome da pessoa: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Ano de nascimento: ");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            p.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
